7243e71ef7ca53d822e826817e17d5c57b05dbbd
Teezolusegun <teezolusegun@gmail.com>
2019-06-19 12:16:48 +0100

    My first react website, looks very awesome

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+const Header = ()=>{
+    return(
+        <h2>TODO_APP</h2>
+    )
+}
+export default Header;
---
+++ src/TodoItems.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+const TodoItems = ()=>{
+    return(
+        <h2>I render Individual todo Items</h2>
+    )
+}
+export default TodoItems;
