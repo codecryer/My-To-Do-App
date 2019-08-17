77dae8b39b3393608d1d3697eb8a71946e2684ae
codecryer <samuel.nzekwe2050@gmail.com>
2019-08-17 15:25:57 +0100

    Updates

---
+++ precache-manifest.27ddfdaa4dc82406962be9a35f5ff9fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b973dd1d27e59309ef16638022dce5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "126f45deacfe916684ee",
+    "url": "/myapp/static/css/main.59a38b84.chunk.css"
+  },
+  {
+    "revision": "b948f7a952360efa585c",
+    "url": "/myapp/static/js/2.a18d2f54.chunk.js"
+  },
+  {
+    "revision": "126f45deacfe916684ee",
+    "url": "/myapp/static/js/main.77c78ec9.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
